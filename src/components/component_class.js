5cb9f2ffbb756dba25527a62ab1dad37fa25a3e4
Joven Pableo <jpableo688@gmail.com>
2018-09-10 01:14:34 -0700

    react boilerplate done with scss config

---
+++ src/components/component_class.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Example2 extends Component {
+	render(){
+		return <div>Example2</div>;
+	}
+}
+
+export default Example2;
---
+++ src/components/component_class_state.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Example3 extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {};
+	}
+
+	render(){
+		return <div>Example3</div>;
+	}
+}
+
+export default Example3;
